58f5b49704b865a729b666eb4abb8394ea91c2af
Ben Richey <b.richeypg@gmail.com>
2023-02-23 14:45:23 -0500

    Add profiler for audio read method
    This profiling code uses a similar technique to read in audio data to
    what is currently used in the SPRD application. Use this to determine
    approximately how long it actually takes for this method to read 1
    seconds worth of audio data.

---
+++ analogReadLatency/analogReadLatency.ino
@@ -0,0 +1,29 @@
+#define ANALOG_PIN A0
+
+//unsigned long sampling_period_us = (1.0 / 16000) * pow(10.0, 6);
+unsigned long sampling_period_us = 51;
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println(sampling_period_us);
+
+}
+
+void audio_read() {
+  for (int i = 0; i < 16000; i++) {
+    unsigned long new_time = micros();
+    uint8_t val = analogRead(ANALOG_PIN);
+    while(micros() < (new_time + sampling_period_us));
+  }
+}
+
+void loop() {
+
+  unsigned long start, duration;
+  start = millis();
+  audio_read();
+  duration = millis() - start;
+  Serial.println(duration);
+
+}
