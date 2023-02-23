1dd431ee18c67df773a3d3d49ec10d028b20b52d
Ben Richey <b.richeypg@gmail.com>
2023-02-23 14:47:12 -0500

    Record audio from Arduino and store as .wav
    The audio_recorder directory contains an Arduino sketch that prompts the
    user to type in a 'y' if they wish to record 1 seconds-worth of audio
    from an analog pin on the board. It is assumed that a microphone is
    connected to that analog pin. The recorded data will be printed to the
    serial terminal in csv format.
    
    The CSV_to_WAV folder contains a simple C++ project that allows the user
    to convert the csv audio data acquired from the Arduino into a .wav file
    that can be listened to on a desktop. You must copy the csv data from
    the serial terminal to a file on your desktop. Then, you can run the
    csv_to_wav.exe (assuming you've already built this by calling 'make').
    This will prompt you to enter in the csv file's name and also where
    you'd like the .wav file to be stored.
    
    Please note: the csv file must be exactly one line where every data
    point is separated by a single comma and no spaces. It won't work
    otherwise.

---
+++ audio_recorder/CSV_to_WAV/csv_to_wav.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "AudioFile.h"
+
+#define NUM_CHANNELS 2
+#define SAMPLING_FREQ 16000
+#define BIT_DEPTH 16
+
+
+int main()
+{
+    // Get csv file name from user
+    std::cout << "Enter csv filename to convert to wav: ";
+    if (std::cin.peek() == '\n') std::cin.ignore();
+    std::string filename;
+    getline(std::cin, filename);
+
+    // Open given file and load data into vector
+    std::vector<float> raw_data;
+    std::fstream csv_file(filename, std::ios::in);
+    if (csv_file.is_open()) {
+        std::string line, datum;
+        getline(csv_file, line);
+        std::stringstream str(line);
+        while(getline(str, datum, ',')) {
+            raw_data.push_back(std::stof(datum));
+        }
+        csv_file.close();
+    } else {
+        std::cout << "Error: Could not open file." << std::endl;
+        return -1;
+    }
+
+    // Check we have at least 1s worth of audio data
+    if (raw_data.size() < SAMPLING_FREQ) {
+        std::cout << "Error: Not enough data in CSV file." << std::endl;
+        return -1;
+    }
+
+    // Create audio file and load audio data into it
+    AudioFile<float> a_file;
+    a_file.setNumChannels(NUM_CHANNELS);
+    a_file.setNumSamplesPerChannel(SAMPLING_FREQ);
+    a_file.setSampleRate(SAMPLING_FREQ);
+    a_file.setBitDepth(BIT_DEPTH);
+    for (int channel = 0; channel < a_file.getNumChannels(); channel++) {
+        for (int i = 0; i < a_file.getNumSamplesPerChannel(); i++) {
+            a_file.samples[channel][i] = raw_data[i];
+        }
+    }
+
+
+    // Prompt user to enter where they want to save the file
+    std::cout << "Enter file name to store .wav file to: ";
+    if (std::cin.peek() == '\n') std::cin.ignore();
+    getline(std::cin, filename);
+    a_file.save(filename);
+
+    return 0;
+}
---
+++ audio_recorder/audio_recorder.ino
@@ -0,0 +1,52 @@
+#define ANALOG_PIN A0
+
+
+const int AUDIO_SAMPLE_FREQUENCY = 16000;
+const int ADC_RES = 12;
+const int RECORDING_DURATION_S = 1;
+const int AUDIO_CAPTURE_BUFFER_SIZE = AUDIO_SAMPLE_FREQUENCY * RECORDING_DURATION_S;
+int16_t audio_buffer[AUDIO_CAPTURE_BUFFER_SIZE];
+// Sampling period in us -> subtract 5 to account for overhead.
+unsigned long SAMPLING_PERIOD_US = ((1.0 / AUDIO_SAMPLE_FREQUENCY) * pow(10.0, 6)) - 5;
+
+/*
+* Reads <size> bytes of data from <ANALOG_PIN> using an
+* <ADC_RESOLUTION>-bit ADC at a sampling rate of <AUDIO_SAMPLE_FREQUENCY>
+* to <buffer>.
+*/
+int audio_read(int16_t *buffer, size_t size) {
+  for (int i = 0; i < size; i++) {
+    unsigned long new_time = micros();
+    uint16_t val = analogRead(ANALOG_PIN);
+    buffer[i] = val;
+    while(micros() < (new_time + SAMPLING_PERIOD_US));
+  }
+  return size;
+}
+
+void setup() {
+  // Initialize serial connection for logging
+  Serial.begin(9600);
+  // Set the ADC resolution
+  analogReadResolution(ADC_RES);
+}
+
+void loop() {
+  Serial.print("Record 1s of audio data? (y/n): ");
+  while(!Serial.available()) {
+    delay(20);
+  }
+  Serial.println("");
+  char input = Serial.read();
+  if (input == 'y') {
+    Serial.println("Recording...");
+    audio_read(audio_buffer, AUDIO_CAPTURE_BUFFER_SIZE);
+    Serial.println("Done.");
+    Serial.println("Outputting raw CSV audio data:");
+    for (int i = 0; i < AUDIO_CAPTURE_BUFFER_SIZE - 1; i++) {
+      Serial.print((float) audio_buffer[i]);
+      Serial.print(",");
+    }
+    Serial.println((float) audio_buffer[AUDIO_CAPTURE_BUFFER_SIZE - 1]);
+  }
+}
