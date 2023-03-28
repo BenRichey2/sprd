9b7c0a4678224cd47fff6d9a24c9a2b8a9b71c47
Ben Richey <b.richeypg@gmail.com>
2023-03-28 19:43:33 -0400

    Increase training epochs to 20k
    Train the model for 20k epochs rather than 15k in an attempt to improve
    performance.

--- model_training/save_model.sh
+++ model_training/save_model.sh
@@ -4,7 +4,7 @@ PREPROCESS="micro"
 MODEL_ARCHITECTURE="tiny_conv"
 TRAIN_DIR="/root/train/"
 MODEL_DIR="/root/models/"
-START_CHECKPOINT="15000"
+START_CHECKPOINT="20000"
 SAVE_FORMAT="saved_model"
 
 python3 tensorflow/tensorflow/examples/speech_commands/freeze.py --wanted_words=$WANTED_WORDS --window_stride_ms=$WINDOW_STRIDE --preprocess=$PREPROCESS --model_architecture=$MODEL_ARCHITECTURE --start_checkpoint=$TRAIN_DIR$MODEL_ARCHITECTURE".ckpt-"$START_CHECKPOINT --save_format=$SAVE_FORMAT --output_file=$MODEL_DIR$SAVE_FORMAT
